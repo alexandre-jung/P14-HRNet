8e80842ad4f80966d38810a664ef156fb2d726f6
Alexandre Jung <jungalexandre@ymail.com>
2023-06-02 16:50:08 +0200

    ⚰️ icons: remove Codux board

--- libs/icons/src/_codux/boards/icon/icon.board.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import { Icon } from '../../../Icon/icon';
-
-export default createBoard({
-    name: 'Icon',
-    Board: () => <Icon name='Calendar' />,
-});
